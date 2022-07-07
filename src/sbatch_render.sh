2ad7f971015fa793680a54287627b8cf6e33da21
Ying Sheng <sqy1415@gmail.com>
2022-07-07 02:44:50 -0700

    minor

--- src/sbatch_render.sh
+++ src/sbatch_render.sh
@@ -8,6 +8,6 @@
 eval "$(conda shell.bash hook)"
 conda activate torchnew
 
-python render.py -path2data /viscam/u/ying1123/language2pose/src/save/model/exp_42_cpk_jl2p_model_Seq2SeqConditioned9_time_16_chunks_1\
+python render.py -path2data /viscam/u/ying1123/language2pose/src/save/model/exp_46_cpk_jl2p_model_Seq2SeqConditioned9_time_16_chunks_1\
                  -render_list subsets/render_list
 
